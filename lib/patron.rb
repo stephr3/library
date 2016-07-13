7c6811b89d9e8c6f9b1794cb274fb7636f1d82e0
Molly McGlone and Stephanie Gurung <molly@>
2016-07-13 14:31:13 -0700

    pass specs Patron.find_by_name, .find_by_patron_id, #update, #delete

--- lib/patron.rb
+++ lib/patron.rb
@@ -29,4 +29,38 @@ class Patron
     result = DB.exec("INSERT INTO patrons (name, phone) VALUES ('#{@name}', '#{@phone}') RETURNING patron_id;")
     @patron_id = result.first['patron_id'].to_i()
   end
+
+  # NEW STUFF
+
+  define_singleton_method(:find_by_name) do |name|
+    returned_patrons = DB.exec("SELECT * FROM patrons WHERE name = '#{name}';")
+    patrons = []
+    returned_patrons.each do |patron|
+      patron_id = patron['patron_id'].to_i
+      name = patron['name']
+      phone = patron['phone']
+      patrons.push(Patron.new({:patron_id => patron_id, :name => name, :phone => phone}))
+    end
+    patrons
+  end
+
+  define_singleton_method(:find_by_patron_id) do |patron_id|
+    returned_patron = DB.exec("SELECT * FROM patrons WHERE patron_id =#{patron_id};").first()
+    patron_id = returned_patron['patron_id'].to_i
+    name = returned_patron['name']
+    phone = returned_patron['phone']
+    Patron.new({:patron_id => patron_id, :name => name, :phone => phone})
+  end
+
+  define_method(:update) do |attributes|
+    @name = attributes.fetch(:name)
+    @phone = attributes.fetch(:phone)
+    @patron_id = self.patron_id()
+    DB.exec("UPDATE patrons SET name = '#{@name}' WHERE patron_id = #{@patron_id};")
+    DB.exec("UPDATE patrons SET phone = '#{@phone}' WHERE patron_id = #{@patron_id};")
+  end
+
+  define_method(:delete) do
+    DB.exec("DELETE FROM patrons WHERE patron_id = #{self.patron_id};")
+  end
 end
--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -38,4 +38,47 @@ describe(Patron) do
       expect(Patron.all()).to(eq([test_patron]))
     end
   end
+
+  # NEW STUFF
+
+  describe('.find_by_name') do
+    it 'locates patrons with a given name' do
+      test_patron1 = Patron.new({:patron_id => nil, :name => 'Mr. Rogers',:phone => '503-250-2173'})
+      test_patron2 = Patron.new({:patron_id => nil, :name => 'Julia Childs',:phone => '206-345-1198'})
+      test_patron1.save()
+      test_patron2.save()
+      expect(Patron.find_by_name(test_patron1.name())).to(eq([test_patron1]))
+    end
+  end
+
+  describe('.find_by_patron_id') do
+    it 'locates a patron with a given patron_id' do
+      test_patron1 = Patron.new({:patron_id => nil, :name => 'Mr. Rogers',:phone => '503-250-2173'})
+      test_patron2 = Patron.new({:patron_id => nil, :name => 'Julia Childs',:phone => '206-345-1198'})
+      test_patron1.save()
+      test_patron2.save()
+      expect(Patron.find_by_patron_id(test_patron1.patron_id())).to(eq(test_patron1))
+    end
+  end
+
+  describe('#update') do
+    it 'lets the user update the attributes of the patron' do
+      test_patron1 = Patron.new({:patron_id => nil, :name => 'Mr. Rogers',:phone => '503-250-2173'})
+      test_patron1.save()
+      test_patron1.update({:patron_id => nil, :name => 'Fred Rogers',:phone => '971-358-9742'})
+      expect(test_patron1.name()).to(eq('Fred Rogers'))
+      expect(test_patron1.phone()).to(eq('971-358-9742'))
+    end
+  end
+
+  describe('#delete') do
+    it 'lets the user delete a patron' do
+      test_patron1 = Patron.new({:patron_id => nil, :name => 'Mr. Rogers',:phone => '503-250-2173'})
+      test_patron2 = Patron.new({:patron_id => nil, :name => 'Julia Childs',:phone => '206-345-1198'})
+      test_patron1.save()
+      test_patron2.save()
+      test_patron1.delete()
+      expect(Patron.all()).to(eq([test_patron2]))
+    end
+  end
 end
