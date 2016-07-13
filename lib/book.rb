3fa98cc41a28e2a6619517ab7e146885a78e8df1
Molly McGlone and Stephanie Gurung <stephanie@>
2016-07-13 10:44:48 -0700

    pass spec Book#save and Book#==

--- lib/book.rb
+++ lib/book.rb
@@ -1,9 +1,10 @@
 require('pry')
 
 class Book
-  attr_reader(:title, :author_first, :author_last, :year_published)
+  attr_reader(:book_id, :title, :author_first, :author_last, :year_published)
 
   define_method(:initialize) do |attributes|
+    @book_id = attributes[:book_id]
     @title = attributes[:title]
     @author_first = attributes[:author_first]
     @author_last = attributes[:author_last]
@@ -11,7 +12,27 @@ class Book
   end
 
   define_singleton_method(:all) do
-    []
+    returned_books = DB.exec("SELECT * FROM books;")
+    books = []
+    returned_books.each() do |book|
+      book_id = book.fetch("book_id").to_i()
+      title = book.fetch("title")
+      author_first = book.fetch("author_first")
+      author_last = book.fetch("author_last")
+      year_published = book.fetch("year_published")
+      books.push(Book.new({:book_id => book_id, :title => title, :author_first => author_first, :author_last => author_last, :year_published => year_published}))
+    end
+    books
   end
-  
+
+  define_method(:==) do |other|
+    self.title == other.title && self.book_id == other.book_id
+  end
+
+
+  define_method(:save) do
+    result = DB.exec("INSERT INTO books (title, author_first, author_last, year_published) VALUES ('#{@title}', '#{@author_first}', '#{@author_last}', #{@year_published}) RETURNING book_id;")
+    @book_id = result.first['book_id'].to_i()
+  end
+
  end
--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -4,7 +4,7 @@ describe(Book) do
 
   describe('#title') do
     it "returns the title of the book" do
-      test_book = Book.new({:title => '19Q4', :author_first => 'Haruki', :author_last => 'Murakami', :year_published => '2009'})
+      test_book = Book.new({:book_id => nil, :title => '19Q4', :author_first => 'Haruki', :author_last => 'Murakami', :year_published => '2009'})
       expect(test_book.title()).to(eq('19Q4'))
     end
   end
@@ -12,6 +12,22 @@ describe(Book) do
   describe('.all') do
     it 'is an empty array at first' do
       expect(Book.all()).to(eq([]))
+    end
+  end
+
+  describe('#==') do
+    it 'compares two books by title and id to see if they are the same' do
+      test_book1 = Book.new({:book_id => 1, :title => '19Q4', :author_first => 'Haruki', :author_last => 'Murakami', :year_published => '2009'})
+      test_book2 = Book.new({:book_id => 1, :title => '19Q4', :author_first => 'Haruki', :author_last => 'Murakami', :year_published => '2009'})
+      test_book1.eql?(test_book2)
+    end
+  end
+
+  describe('#save') do
+    it 'saves a book' do
+      test_book = Book.new({:book_id => nil, :title => '19Q4', :author_first => 'Haruki', :author_last => 'Murakami', :year_published => '2009'})
+      test_book.save()
+      expect(Book.all()).to(eq([test_book]))
     end
   end
 
